651711dd87f9e6a8d269c08e05ac3c14adccbc20
Kiril Savino <kiril.savino@gmail.com>
2017-06-19 23:14:18 -0400

    fix bug

--- vobject/base.py
+++ vobject/base.py
@@ -1081,7 +1081,7 @@ def readComponents(streamOrString, validate=False, transform=True,
                 vline = textLineToContentLine(line, n)
             except VObjectError as e:
                 if tolerantMultiline and lastName is not None:
-                    lastContentLine = stack.top()[lastName]
+                    lastContentLine = stack.top().contents[lastName]
                     lastContentLine.value += '\n' + line
 
                 elif ignoreUnreadable:
